38ee1728eb79c9eb64628cc498b234a6f6c4c1a8
Your Name <your-email@example.com>
2026-06-04 13:36:54 +0300

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import safarilinklogo from "../assets/safarilinklogo.jpg";
+import safarilinklogo from "../assets/safarilinklogo.png";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
